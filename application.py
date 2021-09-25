c806bd4229b734009859fc6272b6ce45d179ab30
nuraaly345 <kubanura57@gmail.com>
2021-09-25 19:36:59 +0300

    all

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from openpyxl import load_workbook
-
 
  
 app = Flask(__name__)
@@ -9,8 +8,32 @@ app = Flask(__name__)
 def homepage():
     excel = load_workbook('task.xlsx')
     page = excel['Sheet']
-    good = [page['A1'].value, page['A2'].value, page['A3'].value, page['A4'].value]
-    return render_template('index.html', tovar = good)
+    string = page['A']
+    li = []
+    for j in range(len(string)):
+        li.append(string[j].value)
+        
+    return render_template('index.html', tovar = li)
+
+
+@app.route('/add/', methods=["POST"])
+def add():
+    goods = request.form['good']
+    excel = load_workbook('task.xlsx')
+    page = excel.active
+    m_row = page.max_row
+    for i in range(1, m_row + 1):
+        cell_obj = page.cell(row = i, column=1)
+        cell_obj.value = goods
+   
+        
+
+    excel.save('task.xlsx')
+    return "<h1>Инвентарь пополнен</h1> <a href='/'>Домой</a>"
+
+
 
 if __name__ == "__main__":
     app.run()
+
+
